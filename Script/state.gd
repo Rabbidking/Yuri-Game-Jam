2b66deec014213e12b3c9e02a04ccd62b2cf41f8
John Bickel <bickel.john@yahoo.com>
2023-11-04 14:47:38 -0400

    Set more event flags in state, with some descriptions

--- Script/state.gd
+++ Script/state.gd
@@ -10,9 +10,57 @@ var is_dialog_active = false
 
 var items = []
 var collected_items = []
+#==================================================================================================
 
+# day flags for level adjustments
 var day_1_active = true
 var day_2_active = false
 var day_3_active = false
 var day_4_active = false
 var day_5_active = false
+#==================================================================================================
+# night 1 specific flags
+var day_1_intro = true
+var night_1_intro = false
+var first_time_found_gigi = false
+var night_1_end = false
+#==================================================================================================
+# night 2 specific flags
+var day_2_intro = false
+var night_2_intro = false
+var garage_first_check_car = false
+var garage_open_trunk = false
+var night_2_end = false
+#==================================================================================================
+# night 3 specific flags
+var day_3_intro = false
+var night_3_intro = false
+var front_ent_recept_first_check = false
+
+# If this is true, play the password missing dialog. Else, play the dialog where she stops delivery, 
+# then set Gigi to appear at the front entrance.
+var bedroom_laptop_no_password = true 
+
+var living_room_sticky = false
+var gigi_at_entrance = false
+var night_3_end = false
+#==================================================================================================
+# night 4 specific flags
+var day_4_intro = false
+var night_4_intro = false
+
+#This should only be true if we're in night 4 and 5
+var living_room_trapdoor = false
+
+#plays dialog of Demi needing the crowbar
+var trapdoor_no_crowbar = true
+
+#plays dialog when Demi opens the trapdoor
+var trapdoor_with_crowbar = false
+
+#play dialog of Demi finding Gigi in basement
+var gigi_found_in_basement = false
+
+var night_4_end = false
+#==================================================================================================
+# night 5 specific flags

5a4258f105f51b4ac12bd60ea4516b0cec8ca729
BrandonL7ce <59879113+BrandonL7ce@users.noreply.github.com>
2023-11-28 07:34:07 -0500

    Add files via upload

--- Fade_Transition.gd
+++ Fade_Transition.gd
@@ -2,10 +2,31 @@ extends Node2D
 
 signal player_can_move
 
+@export var context : StringName
 @onready var transition = $Transition
 @onready var color_rect = $Transition/ColorRect
 
 func _ready():
+	if context == "kitchen":
+		$Transition/Label.text = "Kitchen"
+	elif context == "living_room":
+		$Transition/Label.text = "Living Room"
+	elif context == "basement":
+		$Transition/Label.text = "Basement"
+	elif context == "bathroom":
+		$Transition/Label.text = "Bathroom"
+	elif context == "bedroom":
+		$Transition/Label.text = "Bedroom"
+	elif context == "entrance":
+		$Transition/Label.text = "Entrance"
+	elif context == "garage":
+		$Transition/Label.text = "Garage"
+	elif context == "hallway_a":
+		$Transition/Label.text = "Hallway A"
+	elif context == "hallway_b":
+		$Transition/Label.text = "Hallway B"
+	elif context == "study":
+		$Transition/Label.text = "Study"
 	$Transition.play("fade_out")
 	$Transition.animation_finished
 	$Timer.start()

b39da0f8b9b25bd20367a953c7de4fcf2f91d6b5
John Bickel <bickel.john@yahoo.com>
2023-12-03 21:19:24 -0500

    Started setting up SettingsSignalBus

---
+++ Script/SettingsSignalBus.gd
@@ -0,0 +1,33 @@
+extends Node
+
+#Graphics and resolution signals
+signal on_window_mode_selected(index: int)
+signal on_resolution_selected(index: int)
+
+#Sound signals
+signal on_master_sound_set(value: float)
+signal on_music_sound_set(value: float)
+signal on_sfx_sound_set(value: float)
+
+#signal for setting the settings dictionary
+signal set_settings_dictionary(settings_dict: Dictionary)
+
+
+# Functions for emitting the signals
+func emit_set_settings_dictionary(settings_dict: Dictionary) -> void:
+	set_settings_dictionary.emit(settings_dict)
+
+func emit_on_window_mode_selected(index: int) -> void:
+	on_window_mode_selected.emit(index)
+	
+func emit_on_resolution_mode_selected(index: int) -> void:
+	on_resolution_selected.emit(index)
+	
+func emit_on_master_sound_set(value: float) -> void:
+	on_master_sound_set.emit(value)
+	
+func emit_on_music_sound_set(value: float) -> void:
+	on_music_sound_set.emit(value)
+	
+func emit_on_sfx_sound_set(value: float) -> void:
+	on_sfx_sound_set.emit(value)
